7b15d172386566fb8dea30e5ffa95792eec7a8c8
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-08-11 08:20:37 +0000

    Update Rev_str.asm

--- ASM/Rev_str.asm
+++ ASM/Rev_str.asm
@@ -1,27 +1,27 @@
-ORG 100h
+org 100h
 jmp start
 
-str: DB 'A', 'S', 'S', 'E','M','B','L','E','R'          
-buf: DB 0,0,0,0,0,0,0,0,0,0Dh,0Ah, 24h
+str: db 'A', 'S', 'S', 'E','M','B','L','E','R'          
+buf: db 0,0,0,0,0,0,0,0,0,0dh,0ah, 24h
 
 start:
-LEA SI, str
-LEA BX, buf
-;MOV CX, 9   
-MOV CX, 25
-ADD BX, CX
-DEC BX      
+lea si, str
+lea bx, buf
+;mov cx, 9   
+mov cx, 25
+add bx, CX
+dec bx      
    
 
 mem1: 
-LODSB   
-mov [BX],AL
-DEC BX
-LOOP mem1
+lodsb   
+mov [bx],al
+dec bx
+loop mem1
 
-MOV  dx, buf
-MOV  ah, 09h 
-INT  21h      
-MOV  ah, 0 
-INT  16h 
-RET
+mov  dx, buf
+mov  ah, 09h 
+int  21h      
+mov  ah, 0 
+int  16h 
+ren
