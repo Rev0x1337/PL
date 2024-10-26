393bf252c073493dedcde2fdddf514905b4dcee7
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-10-26 09:57:20 +0300

    Add files via upload

---
+++ ASM/uptime1.asm
@@ -0,0 +1,32 @@
+
+
+format PE GUI 4.0
+entry start
+
+include 'c:\fasm\include\win32ax.inc'
+
+
+section '.code' code readable executable
+
+start:
+
+         invoke  GetTickCount,0
+
+
+        invoke  ExitProcess,0
+
+
+
+;---------------------------------------------
+
+section '.idata' import data readable writeable
+
+library kernel32,"kernel32.dll",\
+        user32,  "user32.dll",\
+        ntdll,   "ntdll.dll"
+
+include "c:\fasm\INCLUDE\api\kernel32.inc"
+include "c:\fasm\INCLUDE\api\user32.inc"
+
+import  ntdll,\
+        NtQuerySystemInformation,'NtQuerySystemInformation'
