71c17471191ecd0be31617b3c3ecdee8a9b1fce3
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-10-29 07:34:48 +0300

    Update uptime1.asm

--- ASM/uptime1.asm
+++ ASM/uptime1.asm
@@ -24,44 +24,44 @@ buff rb 500
 section '.code' code readable executable
 
 start:
-;       mov     eax,
-        invoke  NtQuerySystemInformation,GET_SYSTEM_TIME_INFORMATION,SystemTime,sizeof.SYSTEM_TIME_INFORMATION,0
+;       mov eax,
+        invoke NtQuerySystemInformation,GET_SYSTEM_TIME_INFORMATION,SystemTime,sizeof.SYSTEM_TIME_INFORMATION,0
 
        
-        mov     eax,dword [SystemTime.liKeSystemTime]
-        mov     edx,dword [SystemTime.liKeSystemTime+4]
+        mov eax,dword [SystemTime.liKeSystemTime]
+        mov edx,dword [SystemTime.liKeSystemTime+4]
         
-        sub     eax,dword [SystemTime.liKeBootTime]
-        sbb     edx,dword [SystemTime.liKeBootTime+4]
+        sub eax,dword [SystemTime.liKeBootTime]
+        sbb edx,dword [SystemTime.liKeBootTime+4]
 
-        invoke  FileTimeToLocalFileTime,SystemTime.liKeBootTime,ftSystemBoot
-        invoke  FileTimeToSystemTime,ftSystemBoot,stSystemBoot
+        invoke FileTimeToLocalFileTime,SystemTime.liKeBootTime,ftSystemBoot
+        invoke FileTimeToSystemTime,ftSystemBoot,stSystemBoot
 
-        xor     eax,eax
+        xor eax,eax
 
-        mov     ax,[stSystemBoot.wSecond]
-        push    eax
+        mov ax,[stSystemBoot.wSecond]
+        push eax
 
-        mov     ax,[stSystemBoot.wMinute]
-        push    eax
+        mov ax,[stSystemBoot.wMinute]
+        push eax
 
-        mov     ax,[stSystemBoot.wHour]
-        push    eax
+        mov ax,[stSystemBoot.wHour]
+        push eax
 
-        mov     ax,[stSystemBoot.wYear]
-        push    eax
+        mov ax,[stSystemBoot.wYear]
+        push eax
 
-        mov     ax,[stSystemBoot.wMonth]
-        push    eax
+        mov ax,[stSystemBoot.wMonth]
+        push eax
 
-        mov     ax,[stSystemBoot.wDay]
-        push    eax
+        mov ax,[stSystemBoot.wDay]
+        push eax
 
-        invoke  wsprintf, buff, mask
+        invoke wsprintf, buff, mask
 
-        invoke  MessageBox,0,buff,head,MB_OK
+        invoke MessageBox,0,buff,head,MB_OK
 
-        invoke  ExitProcess,0
+        invoke ExitProcess,0
 
 mask db 'Date: %02d.%02d.%04d',13,10,'Time: %02d:%02d:%02d',0
 head db 'System started',0
